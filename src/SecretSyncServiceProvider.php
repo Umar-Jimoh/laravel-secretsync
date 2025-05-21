5bb81eacf2757bc62b5318e45efa4cd75303dec1
Umar Jimoh <umarjimoh@hotmail.com>
2025-05-21 18:07:27 +0100

    feat: warn user to resync secrets after optimize command

--- src/SecretSyncServiceProvider.php
+++ src/SecretSyncServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace UmarJimoh\SecretSync;
 
+use Illuminate\Console\Events\CommandStarting;
 use Illuminate\Support\ServiceProvider;
 
 class SecretSyncServiceProvider extends ServiceProvider
@@ -20,6 +21,12 @@ class SecretSyncServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../config/secretsync.php' => config_path('secretsync.php'),
         ], 'config');
+
+        \Illuminate\Support\Facades\Event::listen(CommandStarting::class, function(CommandStarting $event) {
+            if(in_array($event->command, ['optimize'])) {
+                echo "\n⚠️  Secrets may be out of sync. Please run `php artisan secretsync` after `{$event->command}` to refresh them.\n";
+            }
+        });
 
         $this->commands([
             \UmarJimoh\SecretSync\Commands\SecretSync::class,
